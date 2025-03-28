da68050f9afdea8a692e14fdf751863c83950649
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-03-28 09:54:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://eujinnlucashow.github.io/carousel_in_react/",
+  base: "/carousel_in_react/",
   plugins: [react()],
 });
